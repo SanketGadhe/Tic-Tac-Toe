7bcffe6a0bb23074d862ac8b9d8c4886d79dd4a9
SANKET VILAS GADHE <98447853+SanketGadhe@users.noreply.github.com>
2023-12-22 00:28:17 +0530

    script.js

--- script.js
+++ script.js
@@ -17,11 +17,10 @@ const winpattern=[
     [0,4,8],
     [2,4,6]
 ]
-all=[0,1,2,3,4,5,6,7,8]
+
 boxes.forEach((box)=>{
    box.addEventListener('click',()=>{
     if(turnO){
-        console.log(boxes.indexOf(box))
         box.innerText='O'
         turnO=false
     }
